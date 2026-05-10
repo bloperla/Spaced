561991daf82acf41d75cce70b303d19e0632bbca
bloperla <tomfawley@btinternet.com>
2026-05-10 15:22:39 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'spaced-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first for HTML (so updates work), cache-first for assets
+  if (e.request.url.includes('index.html') || e.request.url.endsWith('/')) {
+    e.respondWith(fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
